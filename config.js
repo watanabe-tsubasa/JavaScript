3b53accdb1ead2b3dff75b3d5e93b49f83042ab6
watanabe-tsubasa <t.watanabe423@gmail.com>
2024-01-09 14:39:51 +0900

    upd

---
+++ config.js
@@ -0,0 +1,24 @@
+const config = {
+  // MusicBrainz bot account username & password (optional)
+  // botAccount: { 
+  //   username: 'myUserName_bot',
+  //   password: 'myPassword' 
+  // },
+  
+  // API base URL, default: 'https://musicbrainz.org' (optional)
+  baseUrl: 'https://musicbrainz.org',
+
+  appName: 'my-app',
+  appVersion: '0.1.0',
+
+  // Optional, default: no proxy server
+  // proxy: {
+  //   host: 'localhost',
+  //   port: 8888
+  //  },
+
+  // Your e-mail address, required for submitting ISRCs
+  appMail: 't.watanabe423@gmail.com'
+}
+
+module.exports = { config }
--- test.js
+++ test.js
@@ -1,26 +1,13 @@
-const MusicBrainzApi = require('musicbrainz-api').MusicBrainzApi;
+(async () => {
+  const MusicBrainzApi = (await import('musicbrainz-api')).MusicBrainzApi;
 
-const config = {
-  // MusicBrainz bot account username & password (optional)
-  // botAccount: { 
-  //   username: 'myUserName_bot',
-  //   password: 'myPassword' 
-  // },
-  
-  // API base URL, default: 'https://musicbrainz.org' (optional)
-  baseUrl: 'https://musicbrainz.org',
+  const config = require('./config');
+  const mbApi = new MusicBrainzApi(config);
 
-  appName: 'my-app',
-  appVersion: '0.1.0',
+  const main = async () => {
+    const artist = await mbApi.lookup('artist', 'ab2528d9-719f-4261-8098-21849222a0f2');
+    console.log(artist);
+  }
 
-  // Optional, default: no proxy server
-  // proxy: {
-  //   host: 'localhost',
-  //   port: 8888
-  //  },
-
-  // Your e-mail address, required for submitting ISRCs
-  appMail: 't.watanabe423@gmail.com'
-}
-
-const mbApi = new MusicbrainzApi(config);
+  main();
+})();
